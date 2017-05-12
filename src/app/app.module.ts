e5d809631cc1feb2001100a61d49c8a799f068e3
Souvik Das <souvik.das@live.in>
2017-05-12 17:06:33 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'home', component: HomeComponent},
+      {path:'', redirectTo:'home',pathMatch:'full'},
+      {path:'**',component:AboutComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
